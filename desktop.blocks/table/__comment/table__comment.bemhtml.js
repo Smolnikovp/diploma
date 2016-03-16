57771d8f7dbe3930e1fc2fd65bd8373feccb9ebc
smolnikovp <smolnikvop@gmail.com>
2016-03-16 12:30:06 +0300

    шаблоны блока table

---
+++ desktop.blocks/table/__comment/table__comment.bemhtml.js
@@ -0,0 +1,19 @@
+block('table-comment').content()(
+    function () {
+        return [
+            {
+                block: 'text',
+                content: 'Отправлен'
+            },
+            {
+                block: 'link',
+                mods: {type: 'inline', theme: 'turquoise'},
+                content: 'DateTime'
+            },
+            {
+                block: 'text',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+            }
+        ]
+    }
+)
---
+++ desktop.blocks/table/__comment/table__comment.deps.js
@@ -0,0 +1,6 @@
+({
+    shouldDeps: [
+        { block: 'link', mods: { type: 'inline', theme: 'turquoise' }},
+        { block: 'text'}
+    ]
+})
